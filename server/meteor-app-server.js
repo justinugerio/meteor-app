1721887a3deb9ef3285b075dcf8a2b65fc84ac05
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-26 01:24:51 -0700

    Testing server debugging
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- server/meteor-app-server.js
+++ server/meteor-app-server.js
@@ -5,11 +5,18 @@
 // Meteor server startup function
 Meteor.startup(function () {
 
+    debugger;     // line used for debugging with 'meteor debug'
+    var a = 10;
+    var b = 12;
+    var c = a + b;
+
+    console.log('Starting up the server, var c = ' + c);
 
 });
 
 // Accounts create user
 Accounts.onCreateUser(function (options, user) {
+    //debugger;
     if (options.profile) {
         user.profile = options.profile;
     }
