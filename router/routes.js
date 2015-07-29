5e6bbe8bca38c9cf6d67ce738cd9661e35c118a3
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-29 00:57:24 -0700

    Added routing examples
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

---
+++ router/routes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by justin on 7/28/15.
+ */
+
+Router.route('/', function () {
+    this.render('home');
+});
+
+Router.route('/about', function () {
+    this.render('about');
+});
+
