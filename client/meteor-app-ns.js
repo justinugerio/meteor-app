e6da90e4b152646e9af47f4eeb2d3387ab744f14
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-30 21:32:09 -0700

    Added template and buttons for cards
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/meteor-app-ns.js
+++ client/meteor-app-ns.js
@@ -6,7 +6,21 @@
 // Global namespace object
 MeteorApp = {};
 
+// Namespace variables
+MeteorApp.list = function () {
+    return ['abc', 'def', 'ghi', 'jkl', 'mno'];
+}
+
+// Meteor sessions
+Session.setDefault('message', 'this is the default session message!');
+
 // For user accounts, only require username
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_ONLY'
+});
+
+Template.listCards.helpers({
+    getList: function () {
+        return MeteorApp.list();
+    }
 });
--- client/meteor-app.js
+++
@@ -1,8 +0,0 @@
-
-// main start function
-$(function () {
-
-    //alert("Hello there!");
-
-});
-
