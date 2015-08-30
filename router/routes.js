8700cd0e1d2837fbf0bf7bbc465b26e37113acf2
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-08-30 02:33:54 -0700

    Updated Routes for Game of Speed
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- router/routes.js
+++ router/routes.js
@@ -2,8 +2,16 @@
  * Created by justin on 7/28/15.
  */
 
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
 Router.route('/', {
     template: 'home'
+});
+
+Router.route('/speed/', {
+    template: 'speed'
 });
 
 Router.route('/about', {
